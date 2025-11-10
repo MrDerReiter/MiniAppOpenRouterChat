d8128a25577fcf33623722ccaefdcda18e6fec1f
DerReiter <riderbcrich2@gmail.com>
2025-11-11 00:55:05 +0300

    Немного сокращён и перекомпонован код в index.js.

--- public/script/index.js
+++ public/script/index.js
@@ -1,29 +1,47 @@
 import { AIAgent, AIRouter } from "./AIAgentJSBundle.js";
 import { token } from "./token.js";
 
+/**
+ * @param {Object} model 
+ * @returns {boolean}
+ */
+function isFree(model) {
+    return model.pricing.prompt === "0" &&
+        model.pricing.completion === "0"
+}
+/**
+ * @param {Object} model 
+ */
+function addModelButton(model) {
+    const button = document.createElement("li");
+    button.textContent = model.name.replace(" (free)", "");
+    button.onclick = () => location.assign(`chat.html?model=${model.id}`);
+
+    selector.append(button);
+}
+/**
+ * @param {Error} error 
+ */
+function handleError(error) {
+    const errorMessage = document.createElement("li");
+    errorMessage.innerHTML =
+        `Что-то пошло не так: <span class="error-message">${error.message}</span>.</br>` +
+        "Проверьте соединение с интернетом и/или попробуйте обновить страницу.";
+    selector.append(errorMessage);
+}
+
 
 const spinner = document.getElementById("spinner");
 const selector = document.getElementById("modelSelector");
 
 try {
     const agent = new AIAgent(token, AIRouter.openRouter);
-    const models = await agent.getAIModels
-        (model => model.pricing.prompt == "0" && model.pricing.completion == "0");
+    const models = await agent.getAIModels(isFree);
 
-    models.forEach(model => {
-        const button = document.createElement("li");
-        button.textContent = model.name.replace(" (free)", "");
-        button.onclick = () => location.assign(`chat.html?model=${model.id}`);
-
-        selector.append(button);
-    });
-} catch (error) {
-    const errorMessage = document.createElement("li");
-    errorMessage.innerHTML =
-        `Что-то пошло не так: <span class="error-message">${error.message}</span>.</br>` +
-        "Проверьте соединение с интернетом и/или попробуйте обновить страницу.";
-    selector.append(errorMessage);
-} finally {
+    models.forEach(addModelButton);
+}
+catch (error) { handleError(error); }
+finally {
     spinner.hidden = true;
     selector.hidden = false;
 }
